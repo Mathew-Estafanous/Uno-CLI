57d14b8b4c4ebe7dea81f14f74f278406c842cd6
TwinsRock88 <mathewestafanous13@gmail.com>
2021-03-13 11:03:27 -0500

    Update Deck class comments.

--- src/main/java/uno/Deck.java
+++ src/main/java/uno/Deck.java
@@ -2,62 +2,95 @@ package uno;
 
 import uno.cards.*;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Collections;
 
 /**
- * sfsfafaf
+ * The Deck class manages the creation of the deck of cards and
+ * dealing the top card whenever needed. Upon instantiating a Deck
+ * object, the deck of cards will automatically generate and shuffled.
+ * The following are the number of each card type that is made by the deck.
+ * </br>
+ * Numeric Cards: 80 (2 of Each Number & Colour)
+ * Action Cards: 24 (2 of Each Colour)
+ * Wild Cards: 8 (4 of Each Wild Type)
  */
 public class Deck {
 
-  //Lists out all the possible card numbers
-  private final static CardType[] NUMERIC_TYPES = new CardType[] { 
-    CardType.ZERO, CardType.ONE, CardType.TWO, 
-    CardType.THREE, CardType.FOUR, CardType.FIVE, 
-    CardType.SIX, CardType.SEVEN, CardType.EIGHT, CardType.NINE 
-  };
+    //List out all the possible card numbers
+    private final static CardType[] NUMERIC_TYPES = new CardType[] {
+        CardType.ZERO, CardType.ONE, CardType.TWO,
+        CardType.THREE, CardType.FOUR, CardType.FIVE,
+        CardType.SIX, CardType.SEVEN, CardType.EIGHT, CardType.NINE
+    };
 
-  //Lists out all the possible colours
-  private final static CardColour[] REGULAR_COLOURS = new CardColour[] { 
-    CardColour.BLUE, CardColour.GREEN, CardColour.RED, CardColour.YELLOW 
-  };
+    //List out all the possible colours
+    private final static CardColour[] REGULAR_COLOURS = new CardColour[] {
+        CardColour.BLUE, CardColour.GREEN, CardColour.RED, CardColour.YELLOW
+    };
 
-  private List<Card> deckOfCards = new ArrayList<Card>();
+    private final List<Card> deckOfCards = new ArrayList<Card>();
 
-  public Deck() {
-    generateNewDeck();
-  }
+    //Constructor that generates the deck of cards when it is called.
+    public Deck() {
+        generateNewDeck();
+    }
 
-  //Creates two of each numerical cards and adds to deck
-  private void generateNewDeck() { 
-      for(CardType type: NUMERIC_TYPES) {
+    /**
+     * Generates an entirely new deck by creating multiple of each type of card
+     * and adding it to the deck of cards list.
+     */
+    private void generateNewDeck() {
+        //Iterate over every numeric type of card.
+        for(CardType type: NUMERIC_TYPES) {
+            //Iterate over each regular colour
+            for(CardColour colour: REGULAR_COLOURS) {
+                //For-each colour & type generate 2 numeric cards.
+                deckOfCards.addAll(Arrays.asList(
+                    new Card(colour, type),
+                    new Card(colour, type)
+                ));
+            }
+        }
+
+        //Iterate over each regular colour.
         for(CardColour colour: REGULAR_COLOURS) {
-          deckOfCards.add(new Card(colour, type));
-          deckOfCards.add(new Card(colour, type));
+            //Iterate 2 times for each colour.
+            for(int i = 0; i < 2; i++) {
+                //Add each action card to the deck every iteration.
+                deckOfCards.addAll(Arrays.asList(
+                    new DrawTwoCard(colour),
+                    new SkipCard(colour),
+                    new SwitchCard(colour)
+                ));
+            }
         }
-      }
 
-      //Create two of each ActionCard and adds to deck
-      for(CardColour colour: REGULAR_COLOURS) {
-        for(int i = 0; i < 2; i++) {
-          deckOfCards.add(new DrawTwoCard(colour));
-          deckOfCards.add(new SkipCard(colour));
-          deckOfCards.add(new SwitchCard(colour));
+        //Iterate four times and add wild card types to the deck each time.
+        for(int i = 0; i < 4; i++){
+            deckOfCards.addAll(Arrays.asList(
+                    new WildCard(),
+                    new WildDrawFourCard()
+            ));
         }
-      }
-      
-      //Create 4 of each Wild Card and adds to deck
-      for(int i = 0; i < 4; i++){
-        deckOfCards.add(new WildCard());
-        deckOfCards.add(new WildDrawFourCard());
-      }
 
-      Collections.shuffle(deckOfCards);
-  }
+        //Use the collections library to shuffle the list.
+        Collections.shuffle(deckOfCards);
+    }
 
-  //This method deals a card
-  public Card dealCard() {
-    return deckOfCards.remove(0);
-  }
+    /**
+     * This method will return the top card in the deck when called and
+     * remove it from the current deck. If the deck is empty, then
+     * the method will return a null object.
+     */
+    public Card dealCard() {
+        //Check to see if deck is empty. If it is, return null.
+        if(deckOfCards.size() == 0) {
+            return null;
+        }
+        //Call remove on the list and return that card.
+        return deckOfCards.remove(0);
+    }
 }
