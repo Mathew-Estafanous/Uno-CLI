001a3c2e78f37afd083e541945d6ae848036cc5a
TwinsRock88 <mathew.estafanous21@ycdsbk12.ca>
2021-03-24 13:48:05 +0000

    Working on the player

--- src/main/java/uno/characters/AIPlayer.java
+++ src/main/java/uno/characters/AIPlayer.java
@@ -2,6 +2,8 @@ package uno.characters;
 
 import uno.cards.Card;
 
+import static uno.cards.CardColour.*;
+
 public class AIPlayer extends Player {
 
     public AIPlayer(String name) {
@@ -10,6 +12,21 @@ public class AIPlayer extends Player {
 
     @Override
     public Card chooseCard() {
-        return null;
+        int choice = (Math.random() * cardHand.size()) + 1;
+        
+        Card card = cardHand.get(choice);
+        if(card.getColour() == WILD) {
+            choice = Math.random() * 4;
+            if(choice == 1) {
+              card.setColour(GREEN);
+            } else if(choice == 2) {
+              card.setColour(RED);
+            } else if(choice == 3) {
+              card.setColour(YELLOW);
+            } else {
+              card.setColour(BLUE);
+            }
+        }
+        return card;
     }
 }
